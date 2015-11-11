477d91bbb4d5e971b73400bdd9076e5692566063
James Prendergast <jamprendergast@gmail.com>
2015-11-11 12:41:53 +0000

    Initial commit

---
+++ app/src/main/java/ie/dit/james/lab8_databases_pt2/DBManager.java
@@ -0,0 +1,101 @@
+package ie.dit.james.lab8_databases_pt2;
+
+/**
+ * Created by James on 05/11/2015.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.SQLException;
+
+
+public class DBManager {
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "TaskList";
+
+    private static final String TABLE_TASKS = "Tasks";
+
+    private static final String KEY_ID = "_id";
+    private static final String KEY_TASK_NAME = "name";
+    private static final String KEY_TASK_DESCRIPTION = "description";
+    private static final String KEY_TASK_STATUS = "status";
+
+    private static final String CREATE_TASKS_TABLE = "CREATE TABLE Tasks (_id INTEGER PRIMARY KEY autoincrement, name TEXT, description TEXT, status TEXT);";
+
+    private final Context context;
+    private MyDatabaseHelper DBHelper;
+    private SQLiteDatabase db;
+
+    public DBManager(Context ctx) {
+        this.context = ctx;
+        DBHelper = new MyDatabaseHelper(context);
+    }
+
+    private static class MyDatabaseHelper extends SQLiteOpenHelper {
+
+        public MyDatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_TASKS_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASKS);
+
+            onCreate(db);
+        }
+    }
+
+    public DBManager open() throws SQLException
+    {
+        db = DBHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close()
+    {
+        DBHelper.close();
+    }
+
+    public long insertTask(String name, String desc) {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_TASK_NAME, name);
+        initialValues.put(KEY_TASK_DESCRIPTION, desc);
+        initialValues.put(KEY_TASK_STATUS, "N");
+        return db.insert(TABLE_TASKS, null, initialValues);
+    }
+
+    public Cursor getTask(String query)
+    {
+        Cursor mCursor = db.rawQuery(
+                "SELECT description FROM Tasks WHERE name LIKE " + "'" + query + "';", null);
+
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+
+        return mCursor;
+
+    }
+
+    public Cursor getAll()
+    {
+        Cursor mCursor = db.rawQuery(
+                "SELECT * FROM Tasks;", null);
+
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+
+        return mCursor;
+
+    }
+}
+
---
+++ app/src/main/java/ie/dit/james/lab8_databases_pt2/MainActivity.java
@@ -0,0 +1,102 @@
+package ie.dit.james.lab8_databases_pt2;
+
+import android.os.Bundle;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.database.Cursor;
+
+public class MainActivity extends Activity {
+    DBManager db = new DBManager(this);
+    EditText taskName;
+    EditText taskDesc;
+    EditText taskName2;
+    EditText taskName3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button setButton = (Button) findViewById(R.id.button_submit);
+        Button getButton = (Button) findViewById(R.id.button_retrieve);
+
+        final ListView listTasks = (ListView) findViewById(R.id.listView_tasks);
+
+        setButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something when the button is clicked
+                try {
+                    db.open();
+                    taskName = (EditText) findViewById(R.id.editText_taskname);
+                    taskDesc = (EditText) findViewById(R.id.editText_taskdesc);
+                    db.insertTask(taskName.getText().toString(), taskDesc.getText().toString());
+                    db.close();
+                } catch (Exception ex) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Error opening database";
+                    int duration = Toast.LENGTH_LONG;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        });
+
+        getButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something when the button is clicked
+                //
+                try {
+                    db.open();
+                    Cursor result = db.getAll();
+                    MyCursorAdapter cursorAdapter = new MyCursorAdapter(MainActivity.this, result);
+                    listTasks.setAdapter(cursorAdapter);
+                    db.close();
+
+                    String str = result.getString(result.getColumnIndex("description"));
+
+                    Context context = getApplicationContext();
+                    CharSequence text = str;
+                    int duration = Toast.LENGTH_LONG;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } catch (Exception ex) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Error opening database";
+                    int duration = Toast.LENGTH_LONG;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        });
+
+
+        listTasks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View view, int position, long arg) {
+                Cursor mycursor = (Cursor) av.getItemAtPosition(position);
+                String selection = mycursor.getString(2);
+
+                Context context = getApplicationContext();
+                CharSequence text = selection;
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+    }
+}
